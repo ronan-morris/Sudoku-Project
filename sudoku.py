04154f37989980239acbb5977166e4c12c576b92
ronan-morris <78508701+ronan-morris@users.noreply.github.com>
2022-12-06 14:41:23 -0500

    added end screen

--- sudoku.py
+++ sudoku.py
@@ -5,7 +5,8 @@ from board import Board
 from sudoku_generator import generate_sudoku
 
 def get_button_x(i) -> int:
-    return c.WIDTH/2 + (i-1) * c.BOX_WIDTH * c.CELL_PX - 70
+    """Gets the left-side coordinate for button i out of 3 evenly spaced possibilities"""
+    return c.WIDTH/2 + (i-1) * c.BOX_WIDTH * c.CELL_PX - c.BUTTON_WIDTH // 2
 
 def start_screen(screen):
     """Create a game start screen in pygame"""
@@ -15,22 +16,26 @@ def start_screen(screen):
         screen.blit(imp, (0, 0))
     except FileNotFoundError:
         screen.fill(c.BACK_COLOR)
+
     done = False
     while not done:
         mouse = pygame.mouse.get_pos()
         selected_button = 0
+
         if c.HEIGHT//2 + 2 * c.CELL_PX <= mouse[1] <= c.HEIGHT//2 + c.CELL_PX * 3:
             for i in range(3):
                 button_x_coord = get_button_x(i)
-                if button_x_coord <= mouse[0] <= button_x_coord + 140:
+                if button_x_coord <= mouse[0] <= button_x_coord + c.BUTTON_WIDTH:
                     selected_button = i + 1
+
         for i, difficulty_txt in enumerate(['Easy', 'Medium', 'Hard']):
             if selected_button == i + 1:
                 button_col = c.HOVERED_BUTTON_COL
             else:
                 button_col = c.NORMAL_BUTTON_COL
             button_x_coord = get_button_x(i)
-            pygame.draw.rect(screen, button_col, [button_x_coord, c.HEIGHT//2 + 2 * c.CELL_PX , 140, c.CELL_PX // 2])
+            pygame.draw.rect(screen, button_col,
+                [button_x_coord, c.HEIGHT//2 + 2 * c.CELL_PX , c.BUTTON_WIDTH, c.CELL_PX // 2])
             current_button = button_font.render(difficulty_txt, True, c.BUTTON_TEXT_COL)
             screen.blit(current_button, (button_x_coord, c.HEIGHT//2 + 2 * c.CELL_PX))
 
@@ -72,7 +77,7 @@ def run_game(screen, difficulty):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # Get the coordinates of the mouse click
                 if Board.click(pos):
-                    row_col_pos = list(Board.click(pos))
+                    row_col_pos = list(Board.click(pos))  # type: ignore
                     current_board.select(row_col_pos)
                 elif get_button_x(0) < pos[0] < get_button_x(0) + 140:
                     current_board.reset_to_original()
@@ -107,11 +112,11 @@ def run_game(screen, difficulty):
             buttons_not_drawn = False
             for i, fn_txt in enumerate(['Reset', 'Restart', 'Exit']):
                 button_x_coord = get_button_x(i)
-                if button_x_coord <= pos[0] <= button_x_coord + 140 and pos[1] > c.WIDTH :
+                if button_x_coord <= pos[0] <= button_x_coord + c.BUTTON_WIDTH and pos[1] > c.WIDTH :
                     button_col = c.HOVERED_BUTTON_COL
                 else:
                     button_col = c.NORMAL_BUTTON_COL
-                pygame.draw.rect(screen, button_col, [button_x_coord, int(c.HEIGHT * .925), 140, c.CELL_PX // 2])
+                pygame.draw.rect(screen, button_col, [button_x_coord, int(c.HEIGHT * .925), c.BUTTON_WIDTH, c.CELL_PX // 2])
                 current_button = button_font.render(fn_txt, True, c.BUTTON_TEXT_COL)
                 screen.blit(current_button, (button_x_coord, int(c.HEIGHT * .925)))
 
@@ -120,6 +125,34 @@ def run_game(screen, difficulty):
 
 def game_over(screen, winstate):
     """Create a game over screen in pygame"""
+    try:
+        imp = pygame.image.load(f'Game{["Lost", "Won"][int(winstate)]}.jpeg').convert()
+        imp = pygame.transform.scale(imp, (c.WIDTH, c.HEIGHT))
+        screen.blit(imp, (0, 0))
+    except FileNotFoundError:
+        screen.fill(c.BACK_COLOR)
+
+    done = False
+    button_x_coord = get_button_x(1)
+    while not done:
+        mouse = pygame.mouse.get_pos()
+        button_hovered = (c.HEIGHT//2 + 2 * c.CELL_PX <= mouse[1] <= c.HEIGHT//2 + c.CELL_PX * 3 and
+        button_x_coord <= mouse[0] <= button_x_coord + 140)
+
+        pygame.draw.rect(screen, [c.NORMAL_BUTTON_COL, c.HOVERED_BUTTON_COL][button_hovered],
+            [button_x_coord, c.HEIGHT//2 + 2 * c.CELL_PX , 140, c.CELL_PX // 2])
+        current_button = button_font.render(["Exit", "Restart"][int(winstate)], True, c.BUTTON_TEXT_COL)
+        screen.blit(current_button, (button_x_coord, c.HEIGHT//2 + 2 * c.CELL_PX))
+
+        for event in pygame.event.get():
+            # Check for mouse clicks
+            if event.type == pygame.MOUSEBUTTONDOWN and button_hovered:
+                if not winstate:
+                    start_screen(screen)
+                done = True
+
+        pygame.display.flip()
+
 def main():
     """Run to create a pygame Soduku game"""
     pygame.init()
